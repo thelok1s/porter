bc0e481b77b4596745788c100268c21558781249
lok1s <artempugaev84@gmail.com>
2025-04-11 11:18:42 +0300

    fix aliases on links

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -35,30 +35,57 @@ function splitText(text: string, maxLength: number = 4096): string[] {
 function convertVkLinksToHtml(text: string): string {
   if (!text) return "";
 
+  // [url|title] pattern
   const linkPattern = /\[(?<url>[^[|]+)\|(?<title>[^\]]+)]/g;
+
+  // [#alias|url1|url2] pattern
+  const hashtagLinkPattern =
+    /\[#(?<alias>[^[|]+)\|(?<url1>[^|]+)\|(?<url2>[^\]]+)]/g;
+
   const vkIdPattern = /^(id|club)\d+$/;
   const vkLinkPattern =
     /^(https?:\/\/)?(m\.)?vk\.com(\/[\w\-.~:/?#[\]@&()*+,;%="ёЁа-яА-Я]*)?$/;
 
-  const safeText = text
+  let safeText = text
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;");
 
-  return safeText.replace(linkPattern, (_match, url, title) => {
+  safeText = safeText.replace(
+    hashtagLinkPattern,
+    (_match, _alias, url1, _url2) => {
+      if (vkLinkPattern.test(url1)) {
+        if (!url1.startsWith("http")) {
+          url1 = "https://" + url1;
+        }
+        return `<a href="${url1}">${url1}</a>`;
+      }
+      return url1;
+    },
+  );
+
+  safeText = safeText.replace(linkPattern, (_match, url, title) => {
     if (vkIdPattern.test(url)) {
       url = `https://vk.com/${url}`;
     }
 
     if (vkLinkPattern.test(url)) {
+      if (!url.startsWith("http")) {
+        url = "https://" + url;
+      }
       return `<a href="${url}">${title}</a>`;
     }
 
     return `[${url}|${title}]`;
   });
+
+  return safeText;
 }
 
 function getHtmlLink(url: string, text: string): string {
+  if (url && !url.startsWith("http") && !url.startsWith("//")) {
+    url = "https://" + url;
+  }
   return `<a href="${url}">${text}</a>`;
 }
 
