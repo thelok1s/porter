557106b4b63699c2aa8e9e2c2a24474bad292167
lok1s <artempugaev84@gmail.com>
2025-09-05 21:20:15 +0300

    chore(package): setup eslint and workflows

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["eslint.config.mjs"] },
+  { ignores: ["eslint.config.mjs", "dist/**", "build/**", "coverage/**"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
+];
